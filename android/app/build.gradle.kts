0dd797170d37e6d402220f5743363ebe5a60060f
SUBIN <subinps57@gmail.com>
2026-02-13 18:57:39 +0530

    chore(release): set version to 1.0.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.telegramtv"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.5"
+        versionCode = 6
+        versionName = "1.0.1"
 
         val serverUrl = localProperties.getProperty("TELEGRAM_TV_SERVER_URL", "")
         buildConfigField("String", "DEFAULT_SERVER_URL", "\"$serverUrl\"")
